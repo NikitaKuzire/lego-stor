d9f2dffa1d03af794769cbaa01093cfd416c7790
NikitaKuzire <146757193+NikitaKuzire@users.noreply.github.com>
2025-03-12 20:09:14 +0300

    Create script4.js

---
+++ script4.js
@@ -0,0 +1,131 @@
+let imgAxcer = document.querySelector(".axcer")
+let btn = document.querySelector(".knop")
+let imgGolova = document.querySelector(".gol")
+let btntwo = document.querySelector(".knoptwo")
+let imgTilo = document.querySelector(".tilo")
+let btnthree = document.querySelector(".knopthree")
+let imgNogi = document.querySelector(".tel")
+let btnfour = document.querySelector(".knopfour")
+let btnfive = document.querySelector(".knopfive")
+let btnsix = document.querySelector(".knopsix")
+let btnseven = document.querySelector(".knopseven")
+let btneight = document.querySelector(".knopeight")
+let btnnine = document.querySelector(".knopnine")
+
+let btd  = document.querySelector(".dragon")
+let bto = document.querySelector(".owl")
+let btw = document.querySelector(".wolf")
+
+let btnten = document.querySelector(".knopten")
+let btneleven = document.querySelector(".knopeleven")
+let btntwelve = document.querySelector(".knoptwelve")
+let btnthirteen = document.querySelector(".knopthirteen")
+let btnfourteen = document.querySelector(".knopfourteen")
+let btnfifteen = document.querySelector(".knopfifteen")
+
+btnten.addEventListener('click', function() {
+ 
+imgGolova.src = "https://i.ibb.co/ns1gRxkW/photo-2025-03-11-18-46-43-3-removebg-preview.png"
+ })
+btntwelve.addEventListener('click', function() {
+    
+imgTilo.src = "https://i.ibb.co/23Qt5d8J/photo-2025-03-11-18-46-43-2-removebg-preview.png"
+
+}) 
+
+  btnfourteen.addEventListener('click', function() {
+    
+imgNogi.src = "https://i.ibb.co/vRvrTfC/photo-2025-03-11-18-46-43-removebg-preview-1.png"
+
+}) 
+
+btneleven.addEventListener('click', function() {
+ 
+imgGolova.src = "https://i.ibb.co/q346xpjn/photo-2025-03-11-18-46-43-6-removebg-preview.png"
+ })
+ btnthirteen.addEventListener('click', function() {
+    
+imgTilo.src = "https://i.ibb.co/SDY9V0Ny/photo-2025-03-11-18-46-43-5-removebg-preview.png"
+
+}) 
+
+   btnfifteen.addEventListener('click', function() {
+    
+imgNogi.src = "https://i.ibb.co/27MFxBw0/photo-2025-03-11-18-46-43-4-removebg-preview.png"
+
+}) 
+
+btn.addEventListener('click', function() {
+ 
+imgGolova.src = "https://i.ibb.co/C4c0Bqj/photo-2025-03-11-18-46-43-4-removebg-preview.png"
+ })
+
+  btnfour.addEventListener('click', function() {  
+              imgGolova.src = "https://i.ibb.co/ZpQmGByL/photo-2025-03-11-18-46-43-3-removebg-preview.png"
+      })
+
+         btnfive.addEventListener('click', function() {  
+              imgGolova.src = "https://i.ibb.co/JRYsghGm/photo-2025-03-11-18-46-43-3-removebg-preview.png"
+         })
+
+
+            
+
+
+btntwo.addEventListener('click', function() {
+    
+imgTilo.src = "https://i.ibb.co/xKkC7tb5/photo-2025-03-11-18-46-43-removebg-preview.png"
+
+}) 
+btnsix.addEventListener('click', function() {
+    
+imgTilo.src = "https://i.ibb.co/fYrXgXv2/photo-2025-03-11-18-46-43-2-removebg-preview.png"
+
+}) 
+
+           btneight.addEventListener('click', function() {  
+              imgTilo.src = "https://i.ibb.co/LD2BvCCq/photo-2025-03-11-18-46-43-2-removebg-preview.png"
+         })
+
+btnthree.addEventListener('click', function() {
+    
+imgNogi.src = "https://i.ibb.co/NdBrjNVc/photo-2025-03-11-18-46-43-2-removebg-preview.png"
+
+}) 
+
+btnseven.addEventListener('click', function() {
+    
+imgNogi.src = "https://i.ibb.co/MD6SZhQL/photo-2025-03-11-18-46-43-removebg-preview.png"
+
+}) 
+    
+    btnnine.addEventListener('click', function() {
+    
+imgNogi.src = "https://i.ibb.co/ynxmh7YB/photo-2025-03-11-18-46-43-removebg-preview.png"
+
+}) 
+
+
+    btd.addEventListener('click', function() {
+    
+imgAxcer.src = "https://i.ibb.co/hFQ8C15f/photo-2025-03-12-19-15-01-removebg-preview.png"
+
+}) 
+
+    bto.addEventListener('click', function() {
+    
+imgAxcer.src = "https://i.ibb.co/yBpFWBTq/photo-2025-03-12-19-15-01-3-removebg-preview.png"
+
+}) 
+
+
+    btw.addEventListener('click', function() {
+    
+imgAxcer.src = "https://i.ibb.co/xtpJb9X1/photo-2025-03-12-19-15-01-2-removebg-preview.png"
+
+}) 
+
+
+
+
+
