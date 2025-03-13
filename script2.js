33ae27e8bad352a4c81ffd0b7386ee3643ad2771
NikitaKuzire <146757193+NikitaKuzire@users.noreply.github.com>
2025-03-13 19:55:55 +0300

    Update script2.js

--- script2.js
+++ script2.js
@@ -1,6 +1,10 @@
 
 let btn = document.querySelector('.vidprav')
 let container = document.querySelector(".hh")
+let btntwo = document.querySelector('.texto')
+let btnthree = document.querySelector('.texti')
+
+
 
 btn.addEventListener('click', function() {
 
@@ -20,4 +24,25 @@ newElement.style.borderRadius = "3px"
 newElement.style.border = "2px solid #8C58E0"
 newElement.style.padding = "3px"
 newElement.style.display = " inline-block"
+
+
+
 })
+
+
+
+
+btntwo.addEventListener('click', function() {
+btntwo.style.color = "#39ff75"
+   btnthree.style.color = "#8e95f7"
+container.style.textAlign = "left"
+
+    })
+
+   btnthree.addEventListener('click', function() {
+    
+    container.style.textAlign = " right"
+    btnthree.style.color = "#39ff75"
+   btntwo.style.color = "#8e95f7"
+
+    })
